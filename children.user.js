0237d8e76789ce86dff107d9399fa18f57254bf6
yulvil <yulvil@users.noreply.github.com>
2019-01-08 16:54:44 -0800

    Create children.user.js

---
+++ children.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         wikitree children
+// @namespace    https://github.com/yulvil/wikitree
+// @version      0.2
+// @description  Put siblings/children on separate lines
+// @author       yulvil
+// @match        http*://www.wikitree.com/wiki/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/yulvil/wikitree/master/children.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var z = document.querySelectorAll("[itemprop=children]");
+
+    for (let i=0; i<z.length; i++) {
+        z[i].prepend(`${i+1}. `);
+        z[i].prepend(document.createElement("div"));
+    }
+
+    z = document.querySelectorAll("[itemprop=sibling]");
+
+    for (let i=0; i<z.length; i++) {
+        z[i].prepend(`${i+1}. `);
+        z[i].prepend(document.createElement("div"));
+    }
+
+})();
